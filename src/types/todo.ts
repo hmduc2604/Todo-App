e6480e47f6161fb71ab86531be7d2dc80278dcf7
hmduc2604 <DucHM.B22CN225@stu.ptit.edu.vn>
2026-07-23 11:28:02 +0700

    Finale

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -8,3 +8,31 @@ export interface TodoState {
   todos: Todo[];
   filter: FilterType;
 }
+export interface TodoState {
+  todos: Todo[];
+  filter: FilterType;
+}
+export type TodoAction =
+  | {
+      type: "ADD_TODO";
+      payload: string;
+    }
+  | {
+      type: "DELETE_TODO";
+      payload: string;
+    }
+  | {
+      type: "TOGGLE_TODO";
+      payload: string;
+    }
+  | {
+      type: "EDIT_TODO";
+      payload: {
+        id: string;
+        title: string;
+      };
+    }
+  | {
+      type: "SET_FILTER";
+      payload: FilterType;
+    };
